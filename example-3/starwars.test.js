dac70c55b6dde89316dc35d089545e9fc05c515e
Matt Farmer <matt.farmer@wizeline.com>
2019-06-07 11:27:57 -0700

    Add stub data

--- example-3/starwars.test.js
+++ example-3/starwars.test.js
@@ -1,5 +1,12 @@
 const starwars = require('./starwars');
 
+jest.mock('node-fetch', () => {
+  return () => ({
+    // eslint-disable-next-line global-require
+    json: () => require('./__stubs__/character-response'),
+  });
+});
+
 describe('example-3', () => {
   describe('starwars', () => {
     it('is a function', () => {
