1665325f4e62828a0512573618fe32f6a75b68ab
Matt Farmer <matt.farmer@wizeline.com>
2019-06-07 09:05:22 -0700

    Mock too much

--- example-1/hash.test.js
+++ example-1/hash.test.js
@@ -1,9 +1,19 @@
 const hash = require('./hash');
 
+jest.mock('ebg13', () => () => 'encoded-string');
+
 describe('example-1', () => {
   describe('hash', () => {
     it('is a function', () => {
       expect(hash).toEqual(expect.any(Function));
+    });
+
+    it('encodes a string', () => {
+      const input = 'abcdef1234!@#$';
+
+      const result = hash(input);
+
+      expect(result).toEqual('encoded-string');
     });
   });
 });
