64cc9a05669611552e2e7582d560c178885714a0
Matt Farmer <matt.farmer@wizeline.com>
2019-06-07 10:44:08 -0700

    Change implementation

--- example-1/hash.js
+++ example-1/hash.js
@@ -1,7 +1,31 @@
-const rot13 = require('ebg13');
+const R = require('ramda');
 
-function hash(input) {
-  return rot13(input);
-}
+const aLowerCode = 'a'.charCodeAt();
+const zLowerCode = 'z'.charCodeAt();
+const aUpperCode = 'A'.charCodeAt();
+const zUpperCode = 'Z'.charCodeAt();
+
+const between = (lower, higher) => R.allPass([R.lte(lower), R.gte(higher)]);
+const isLower = between(aLowerCode, zLowerCode);
+const isUpper = between(aUpperCode, zUpperCode);
+
+const hash = R.pipe(
+  R.map((char) => {
+    const charCode = char.charCodeAt();
+    if (isLower(charCode)) {
+      return String.fromCodePoint(
+        ((charCode - aLowerCode + 13) % 26) + aLowerCode,
+      );
+    }
+    if (isUpper(charCode)) {
+      return String.fromCodePoint(
+        ((charCode - aUpperCode + 13) % 26) + aUpperCode,
+      );
+    }
+
+    return char;
+  }),
+  R.join(''),
+);
 
 module.exports = hash;
